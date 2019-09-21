4b6a34f4f98be8e2ad4169e16f42598c50833137
prihuda <rqul22@gmail.com>
2019-09-21 20:00:29 +0700

    Initial commit

---
+++ app/Question.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+    
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute(){
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+}
